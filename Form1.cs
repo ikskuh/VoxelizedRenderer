170461fd8672ae454212591e3814fd92abedca3e
Felix Queißner <felix@ib-queissner.de>
2013-03-26 04:28:16 +0100

    Initial release

---
+++ Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using OCL;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace VoxelizedRenderer
+{
+	[StructLayout(LayoutKind.Explicit)]
+	public struct Camera
+	{
+		[FieldOffset(0)]
+		public Float3 Position;
+
+		[FieldOffset(16)]
+		public Float3 Direction;
+
+		[FieldOffset(32)]
+		public Float3 Right;
+
+		[FieldOffset(48)]
+		public Float3 top;
+
+		[FieldOffset(64)]
+		public float FocalLength;
+
+		[FieldOffset(68)]
+		public float Aspect;
+
+		public void Update()
+		{
+			this.Right = Float3.Cross(this.Direction, Float3.UnitY);
+			this.Right.Normalize();
+			this.top = Float3.Cross(this.Direction, Right);
+			this.top.Normalize();
+		}
+	}
+
+	public partial class Form1 : Form
+	{
+		[DllImport("user32.dll")]
+		private static extern short GetAsyncKeyState(Keys vKey);
+
+
+		byte[] bitmapBuffer;
+		Camera camera;
+		Memory cameraBuffer;
+		Context context;
+		Device device;
+		Kernel kernel;
+		CommandQueue queue;
+		Bitmap renderTarget;
+		Memory target;
+		Memory world;
+
+		int[] worldData;
+		int sizeX = 256;
+		int sizeY = 128;
+		int sizeZ = 256;
+
+		public Form1()
+		{
+			InitializeComponent();
+			this.ClientSize = new Size(800, 600);
+			this.DoubleBuffered = true;
+		}
+
+		void setWorld(int x, int y, int z, Byte4 value)
+		{
+			setWorld(x, y, z, BitConverter.ToInt32(value.GetBytes(), 0));
+		}
+
+		void setWorld(int x, int y, int z, int value)
+		{
+			int offset = sizeX * sizeY * z + sizeX * y + x;
+			worldData[offset] = value;
+		}
+
+		Byte4 getWorld(int x, int y, int z)
+		{
+			int offset = sizeX * sizeY * z + sizeX * y + x;
+			return new Byte4(worldData[offset]);
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			foreach (var platform in OpenCL.GetPlatforms())
+			{
+				var devices = platform.GetDevices(DeviceType.GPU);
+				if (devices.Length > 0)
+					this.device = devices[0];
+			}
+			context = Context.Create(device);
+			queue = context.CreateQueue(device, CommandQueueProperties.None);
+			renderTarget = new Bitmap(800, 600);
+			this.BackgroundImage = renderTarget;
+			bitmapBuffer = new byte[4 * renderTarget.Width * renderTarget.Height];
+
+			string source = File.ReadAllText("kernel.cu");
+			Program pgm = context.CreateProgram(source);
+			try
+			{
+				pgm.Build("", device);
+				while (pgm.GetBuildStatus(device) == BuildStatus.InProgress) System.Threading.Thread.Sleep(1);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message + ":\n" + pgm.GetBuildLog(device));
+				return;
+			}
+			kernel = pgm.CreateKernel("render");
+
+			camera = new Camera();
+			camera.Position = new Float3(sizeX / 2, sizeY / 2, sizeZ / 2);
+			camera.Direction = new Float3(1, 0, 0);
+			camera.Direction.Normalize();
+			camera.FocalLength = 300;
+			camera.Aspect = 800.0f / 600.0f;
+			camera.Update();
+
+			worldData = new int[sizeX * sizeY * sizeZ];
+
+			Random rnd = new Random();
+			Bitmap bmp = (Bitmap)Bitmap.FromFile("heightmap.png");
+			for (int x = 0; x < sizeX; x++)
+			{
+				for (int z = 0; z < sizeZ; z++)
+				{
+					Color c = bmp.GetPixel(x, z);
+					int height = (int)(0.5 * c.R);
+					for (int y = 0; y < height; y++)
+					{
+						setWorld(x, y, z, Color.Gray.ToArgb());
+					}
+					Color gras = Color.FromArgb(0, rnd.Next(120, 137), 0);
+					setWorld(x, height, z, gras.ToArgb());
+				}
+			}
+
+			int numTrees = rnd.Next(10, 500);
+			for (int i = 0; i < numTrees; i++)
+			{
+				int x = rnd.Next(1, sizeX - 1);
+				int z = rnd.Next(1, sizeZ - 1);
+
+				int sy = 0;
+				for (sy = 0; sy < sizeY - 4; sy++)
+				{
+					if (getWorld(x, sy, z).X == 0)
+						break;
+				}
+
+				int height = rnd.Next(4, 12);
+
+				for (int y = 1; y < 4 + height; y++)
+				{
+					setWorld(x, sy + y, z, Color.Brown.ToArgb());
+
+					if (y > 3)
+					{
+						setWorld(x - 1, sy + y, z, Color.Lime.ToArgb());
+						setWorld(x, sy + y, z - 1, Color.Lime.ToArgb());
+						setWorld(x + 1, sy + y, z, Color.Lime.ToArgb());
+						setWorld(x, sy + y, z + 1, Color.Lime.ToArgb());
+					}
+				}
+				setWorld(x, sy + height + 4, z, Color.Lime.ToArgb());
+			}
+
+			target = context.CreateBuffer(MemoryFlags.ReadOnly | MemoryFlags.CopyHostPtr, bitmapBuffer);
+			cameraBuffer = context.CreateBuffer<Camera>(MemoryFlags.ReadWrite | MemoryFlags.CopyHostPtr, camera);
+			world = context.CreateBuffer(MemoryFlags.ReadOnly | MemoryFlags.CopyHostPtr, worldData);
+
+			base.OnLoad(e);
+
+			timerRender.Start();
+		}
+
+		float rotation = 0;
+
+		private void Render()
+		{
+			kernel.SetArgument(0, target);
+			kernel.SetArgument(1, renderTarget.Width);
+			kernel.SetArgument(2, renderTarget.Height);
+			kernel.SetArgument(3, cameraBuffer);
+			kernel.SetArgument(4, world);
+			kernel.SetArgument(5, sizeX);
+			kernel.SetArgument(6, sizeY);
+			kernel.SetArgument(7, sizeZ);
+
+			queue.EnqueueWriteBuffer<Camera>(
+				cameraBuffer,
+				true,
+				0,
+				camera).Wait();
+
+			queue.EnqueueNDRangeKernel(
+				kernel,
+				2,
+				null,
+				new uint[] { (uint)renderTarget.Width, (uint)renderTarget.Height },
+				null).Wait();
+
+			queue.EnqueueReadBuffer(
+				target,
+				true,
+				0,
+				bitmapBuffer).Wait();
+
+			var bmpLock = renderTarget.LockBits(new Rectangle(0, 0, renderTarget.Width, renderTarget.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+			Marshal.Copy(bitmapBuffer, 0, bmpLock.Scan0, bitmapBuffer.Length);
+			renderTarget.UnlockBits(bmpLock);
+			this.Invalidate();
+		}
+
+		private void timerRender_Tick(object sender, EventArgs e)
+		{
+			timerRender.Stop();
+
+			if (GetAsyncKeyState(Keys.Left) != 0)
+				rotation -= 0.1f;
+			if (GetAsyncKeyState(Keys.Right) != 0)
+				rotation += 0.1f;
+			camera.Direction = new Float3(
+				(float)(Math.Cos(rotation)),
+				0,
+				(float)(Math.Sin(rotation)));
+			camera.Update();
+
+			Float3 offset = new Float3(0, 0, 0);
+			if (GetAsyncKeyState(Keys.W) != 0)
+				camera.Position += camera.Direction;
+			if (GetAsyncKeyState(Keys.S) != 0)
+				camera.Position -= camera.Direction;
+			if (GetAsyncKeyState(Keys.A) != 0)
+				camera.Position -= camera.Right;
+			if (GetAsyncKeyState(Keys.D) != 0)
+				camera.Position += camera.Right;
+
+			this.Render();
+			timerRender.Start();
+		}
+	}
+}
---
+++ kernel.cu
@@ -0,0 +1,201 @@
+﻿
+#pragma OPENCL EXTENSION cl_amd_printf : enable
+
+typedef struct _CAMERA
+{
+	float3 position;
+	float3 direction;
+	float3 right;
+	float3 top;
+	float focalLength;
+	float aspect;
+} CAMERA;
+
+typedef struct _RAY
+{
+	float3 position;
+	float3 direction;
+} RAY;
+
+typedef struct _BOX
+{
+	float3 minimum;
+	float3 maximum;
+} BOX;
+
+RAY getRay(global CAMERA *camera, float x, float y)
+{
+	RAY ray;
+	ray.position = camera->position.xyz;
+	ray.direction = 
+		camera->direction * camera->focalLength + 
+		camera->right * 400 * x * camera->aspect + 
+		camera->top * 400 * y;
+	ray.direction = normalize(ray.direction);
+	return ray;
+}
+
+bool intersect_ray_box(RAY *ray, BOX *box)
+{
+	float dist = 0.0f;
+	float maxValue = FLT_MAX;
+	if (fabs(ray->direction.x) < 1E-06f)
+	{
+		if ((ray->position.x < box->minimum.x) || (ray->position.x > box->maximum.x))
+		{
+			return false;
+		}
+	}
+	else
+	{
+		float num11 = 1.0f / ray->direction.x;
+		float num8 = (box->minimum.x - ray->position.x) * num11;
+		float num7 = (box->maximum.x - ray->position.x) * num11;
+		if (num8 > num7)
+		{
+			float temp = num8;
+			num8 = num7;
+			num7 = temp;
+		}
+		dist = max(num8, dist);
+		maxValue = min(num7, maxValue);
+		if (dist > maxValue)
+		{
+			return false;
+		}
+	}
+	if (fabs(ray->direction.y) < 1E-06f)
+	{
+		if ((ray->position.y < box->minimum.y) || (ray->position.y > box->maximum.y))
+		{
+			return false;
+		}
+	}
+	else
+	{
+		float num10 = 1.0f / ray->direction.y;
+		float num6 = (box->minimum.y - ray->position.y) * num10;
+		float num5 = (box->maximum.y - ray->position.y) * num10;
+		if (num6 > num5)
+		{
+			float temp = num6;
+			num6 = num5;
+			num5 = temp;
+		}
+		dist = max(num6, dist);
+		maxValue = min(num5, maxValue);
+		if (dist > maxValue)
+		{
+			return false;
+		}
+	}
+	if (fabs(ray->direction.z) < 1E-06f)
+	{
+		if ((ray->position.z < box->minimum.z) || (ray->position.z > box->maximum.z))
+		{
+			return false;
+		}
+	}
+	else
+	{
+		float num9 = 1.0f / ray->direction.z;
+		float num4 = (box->minimum.z - ray->position.z) * num9;
+		float num3 = (box->maximum.z - ray->position.z) * num9;
+		if (num4 > num3)
+		{
+			float temp = num4;
+			num4 = num3;
+			num3 = temp;
+		}
+		dist = max(num4, dist);
+		maxValue = min(num3, maxValue);
+		if (dist > maxValue)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+__kernel void render(	global uchar4 *renderTarget, 
+						int width, 
+						int height, 
+						global CAMERA *camera,
+						global uchar4 *world,
+						int sizeX,
+						int sizeY,
+						int sizeZ)
+{
+	const int x = get_global_id(0);
+	const int y = get_global_id(1);
+	if(x >= width || y >= height)
+		return;
+	int id = x + width * y;
+	
+	const float pixx = 2.0f * ((float)x / width) - 1.0f;
+	const float pixy = 2.0f * ((float)y / height) - 1.0f;
+	
+	BOX volume;
+	RAY ray = getRay(camera, pixx, pixy);
+
+	volume.minimum = (float3)(0);
+	volume.maximum = (float3)(sizeX, sizeY, sizeZ);
+	
+	renderTarget[id] = (uchar4)(255, 128, 64, 255);
+	if(!intersect_ray_box(&ray, &volume))
+	{
+		return;
+	}
+	
+	float clip_far = 1600;
+	float stepWidth = clip_far / 16000.0f;
+
+	float3 start = ray.position;
+	float3 step = sign(ray.direction);
+	float3 boundary = start;
+	boundary.x += step.x > 0;
+	boundary.y += step.y > 0;
+	boundary.z += step.z > 0;
+
+	float3 tmax = (boundary - ray.position) / ray.direction;
+	if(isnan(tmax.x)) tmax.x = FLT_MAX;
+	if(isnan(tmax.y)) tmax.y = FLT_MAX;
+	if(isnan(tmax.z)) tmax.z = FLT_MAX;
+
+	float3 tdelta = step / ray.direction;
+	if(isnan(tdelta.x)) tdelta.x = FLT_MAX;
+	if(isnan(tdelta.y)) tdelta.y = FLT_MAX;
+	if(isnan(tdelta.z)) tdelta.z = FLT_MAX;
+
+	int px = (int)(start.x + 0.5f);
+	int py = (int)(start.y + 0.5f);
+	int pz = (int)(start.z + 0.5f);
+
+	for(int i = 0; i < 1000; i++)
+	{
+		if(px >= 0 && py >= 0 && pz >= 0 && px < sizeX && py < sizeY && pz < sizeZ)
+		{
+			int offset = sizeX * sizeY * pz + sizeX * py + px;
+			if(world[offset].w > 127)
+			{
+				renderTarget[id] = world[offset];
+				return;
+			}
+		}
+		if(tmax.x < tmax.y && tmax.x < tmax.z)
+		{
+			px += step.x;
+			tmax.x += tdelta.x;
+		}
+		else if(tmax.y < tmax.z)
+		{
+			py += step.y;
+			tmax.y += tdelta.y;
+		}
+		else
+		{
+			pz += step.z;
+			tmax.z += tdelta.z;
+		}
+	}
+}
